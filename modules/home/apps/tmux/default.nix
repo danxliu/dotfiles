b27c00bbda4c04ff025cac1f6d45ad782f219ba1
Daniel Liu <danxliu@protonmail.com>
2026-08-29 22:05:24 -0700

    feat: tmux don't end copy on yank

--- modules/home/apps/tmux/default.nix
+++ modules/home/apps/tmux/default.nix
@@ -9,6 +9,7 @@
     baseIndex = 1;
     shortcut = "Space";
     shell = "${pkgs.fish}/bin/fish";
+    keyMode = "vi";
 
     extraConfig = ''
       set-option -g allow-rename off
@@ -19,6 +20,7 @@
       set -g status-justify left
       set -g status-style 'fg=blue'
       setw -g mode-style 'fg=black bg=blue'
+      set -g status-position top
       set -g status-right-style 'fg=black bg=blue'
       set -g status-right ' %Y-%m-%d %H:%M %p '
       setw -g window-status-current-style 'fg=black bg=blue'
@@ -26,6 +28,10 @@
       set -g message-style 'fg=black bg=blue bold'
 
       bind r source-file ~/.config/tmux/tmux.conf \; display-message "Config reloaded!"
+
+      bind -T copy-mode-vi v send-keys -X begin-selection
+      bind -T copy-mode-vi y send-keys -X copy-selection
+      bind -T copy-mode-vi MouseDragEnd1Pane send-keys -X copy-selection
 
       bind h select-pane -L
       bind l select-pane -R
@@ -40,6 +46,9 @@
       bind Tab next-window
       bind BTab previous-window
       bind t new-window
+
+      bind '"' split-window -v -c "#{pane_current_path}"
+      bind % split-window -h -c "#{pane_current_path}"
     '';
   };
 }
